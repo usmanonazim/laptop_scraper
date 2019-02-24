88a31cfa942fdf26e200e6892e5dc949d7081efb
UsmanOmar <omarusman830@gmail.com>
2019-02-24 19:11:23 +0000

    Create laptop_data.py

---
+++ laptop_data.py
@@ -0,0 +1,17 @@
+import pandas as pd
+# import matplotlib.pyplot as plt
+laptop_data = pd.read_csv('laptop_data.csv')
+#laptop_data.to_csv('laptop_data.csv', index=False)
+apple_data = laptop_data.loc[laptop_data['Brand'] == 'APPLE']
+brands = []
+# make an array of unique brands
+brands = laptop_data['Brand'].unique()
+for brand in brands:
+    print(laptop_data.loc[laptop_data['Brand'] == brand])
+apple_data.to_csv('apple_data.csv', index=False)
+#print(apple_data.at[0, 'Price'])
+# print(apple_data)
+
+
+# #TODO - make separate tables for each brand
+# #TODO - make histograms for each brand
