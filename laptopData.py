6efcf3685d364bbc680c4b2b465f72e56e4514c9
UsmanOmar <omarusman830@gmail.com>
2019-02-24 14:01:45 +0000

    update

--- laptopData.py
+++ laptopData.py
@@ -3,9 +3,13 @@ import pandas as pd
 laptop_data = pd.read_csv('laptop_data.csv')
 #laptop_data.to_csv('laptop_data.csv', index=False)
 apple_data = laptop_data.loc[laptop_data['Brand'] == 'APPLE']
-
-print(apple_data.at[0, 'Price'])
-print(apple_data)
+brands = []
+brands = laptop_data['Brand'].unique()
+for brand in brands:
+    print(laptop_data.loc[laptop_data['Brand'] == brand])
+# apple_data.to_csv('apple_data.csv')
+#print(apple_data.at[0, 'Price'])
+# print(apple_data)
 
 
 # #TODO - make separate tables for each brand
