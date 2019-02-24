5f7f5f34e2784108ba649edc168e7a54bdd195ed
UsmanOmar <omarusman830@gmail.com>
2019-02-24 13:49:55 +0000

    new table

--- laptopData.py
+++ laptopData.py
@@ -2,14 +2,10 @@ import pandas as pd
 # import matplotlib.pyplot as plt
 laptop_data = pd.read_csv('laptop_data.csv')
 #laptop_data.to_csv('laptop_data.csv', index=False)
-print(laptop_data['Brand'].unique())
-print(laptop_data.loc[laptop_data['Brand']=='APPLE'])
-print(laptop_data.loc[laptop_data['Brand']=='ACER'])
-print(laptop_data.loc[laptop_data['Brand']=='DELL'])
-print(laptop_data.loc[laptop_data['Brand']=='LENOVO'])
-print(laptop_data.loc[laptop_data['Brand']=='LG'])
-print(laptop_data.loc[laptop_data['Brand']=='MICROSOFT'])
+apple_data = laptop_data.loc[laptop_data['Brand'] == 'APPLE']
 
+print(apple_data.at[0, 'Price'])
+print(apple_data)
 
 
 # #TODO - make separate tables for each brand
